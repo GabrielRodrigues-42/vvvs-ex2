559f45eb1ccbb4383fc98726f02d939b70e33dfe
GabrielRodrigues-42 <gabriel.rodrigues@ccc.ufcg.edu.br>
2024-07-30 17:12:17 -0300

    Primeiro teste.

--- src/test/java/br/vvs/gabrielEx2/ticketmanTest.java
+++ src/test/java/br/vvs/gabrielEx2/ticketmanTest.java
@@ -1,12 +1,28 @@
 package br.vvs.gabrielEx2;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ticketmanTest {
 
-    @Test
-    void printAllShows() {
+    @BeforeEach
+    void init() {
+        Calendar dataShow1 = new GregorianCalendar();
+        dataShow1.set(2024, 8, 22);
+        String art1 = "Chappell Roan";
+        int cache1 = 1000;
+        int despesas1 = 1000;
+    }
 
+    @Test
+    void criarShow() {
+        Ticketman ticketman = new Ticketman();
+        ticketman.criarShow(dataShow1, art1, cache1, despesas1, false);
     }
 
 }
