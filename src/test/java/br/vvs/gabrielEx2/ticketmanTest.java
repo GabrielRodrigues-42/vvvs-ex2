e457131980e060db99e8579e3645f57ec4573e6a
GabrielRodrigues-42 <gabriel.rodrigues@ccc.ufcg.edu.br>
2024-07-30 22:21:25 -0300

    Enxugando o código.

--- src/test/java/br/vvs/gabrielEx2/ticketmanTest.java
+++ src/test/java/br/vvs/gabrielEx2/ticketmanTest.java
@@ -1,27 +1,51 @@
 package br.vvs.gabrielEx2;
-import org.junit.Before;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ticketmanTest {
+class TicketmanTest {
+    private Calendar dataShow1;
+    private Calendar dataShow2;
+    private String art1;
+    private String art2;
+    private int cache1;
+    private int cache2;
+    private int despesas1;
+    private int despesas2;
+    //private Ticketman ticketman;
+    private Show show1;
+    private Show show2;
+    private Lote lote1;
 
+    @BeforeEach
+    public void setup() {
+        System.out.println("Once Upon a Time");
+        this.dataShow1 = new GregorianCalendar();
+        dataShow1.set(2024, 0, 22);
+        this.dataShow2 = new GregorianCalendar();
+        dataShow2.set(2024, 11, 25);
+        this.art1 = "Chappell Roan";
+        this.art2 = "WILLOW";
+        this.cache1 = 1000;
+        this.cache2 = 2000;
+        this.despesas1 = 1000;
+        this.despesas2 = 2000;
+        this.show1 = new Show(dataShow1, art1, cache1, despesas1, false);
+        this.show2 = new Show(dataShow2, art2, cache2, despesas2, true);
+        this.lote1 = new Lote("22012024Chappell Roan-0", 25, 10, 65, 0);
+    }
 
     @Test
     void criarShow() {
-        Calendar dataShow1 = new GregorianCalendar();
-        dataShow1.set(2024, 0, 22);
-        String art1 = "Chappell Roan";
-        int cache1 = 1000;
-        int despesas1 = 1000;
+        System.out.println("Criar Show");
         Ticketman ticketman = new Ticketman();
-        Show show = new Show(dataShow1, art1, cache1, despesas1, false);
-        assertTrue(show.equals(ticketman.criarShow(dataShow1, art1, cache1, despesas1, false)));
+        assertTrue(show1.equals(ticketman.criarShow(dataShow1, art1, cache1, despesas1, false)));
         assertEquals(ticketman.getNumeroDeShows(), 1);
         ticketman.criarShow(dataShow1, "WILLOW", cache1, despesas1, false);
         assertEquals(ticketman.getNumeroDeShows(), 2);
@@ -29,32 +53,16 @@ class ticketmanTest {
 
     @Test
     void criarShowDiferente() {
-        Calendar dataShow1 = new GregorianCalendar();
-        dataShow1.set(2024, 0, 22);
-        String art1 = "Chappell Roan";
-        int cache1 = 1000;
-        int despesas1 = 1000;
+        System.out.println("Criar Show Errado");
         Ticketman ticketman = new Ticketman();
-        Show show = new Show(dataShow1, art1, cache1, despesas1, false);
-        assertFalse(show.equals(ticketman.criarShow(dataShow1, "WILLOW", cache1, despesas1, false)));
+        assertFalse(show1.equals(ticketman.criarShow(dataShow1, "WILLOW", cache1, despesas1, false)));
         assertEquals(ticketman.getNumeroDeShows(), 1);
     }
 
     @Test
     void getShowEspecifico() {
-        Calendar dataShow1 = new GregorianCalendar();
-        dataShow1.set(2024, 0, 22);
-        Calendar dataShow2 = new GregorianCalendar();
-        dataShow2.set(2024, 11, 25);
-        String art1 = "Chappell Roan";
-        String art2 = "WILLOW";
-        int cache1 = 1000;
-        int cache2 = 2000;
-        int despesas1 = 1000;
-        int despesas2 = 2000;
+        System.out.println("Get Show Específico");
         Ticketman ticketman = new Ticketman();
-        Show show1 = new Show(dataShow1, art1, cache1, despesas1, false);
-        Show show2 = new Show(dataShow2, art2, cache2, despesas2, true);
         ticketman.criarShow(dataShow1, art1, cache1, despesas1, false);
         ticketman.criarShow(dataShow2, art2, cache2, despesas2, true);
         assertTrue(show1.equals(ticketman.getShow("22012024Chappell Roan")));
@@ -64,23 +72,12 @@ class ticketmanTest {
 
     @Test
     void addLote() {
-        Calendar dataShow1 = new GregorianCalendar();
-        dataShow1.set(2024, 0, 22);
-        Calendar dataShow2 = new GregorianCalendar();
-        dataShow2.set(2024, 11, 25);
-        String art1 = "Chappell Roan";
-        String art2 = "WILLOW";
-        int cache1 = 1000;
-        int cache2 = 2000;
-        int despesas1 = 1000;
-        int despesas2 = 2000;
+        System.out.println("Criar e Adicionar novo Lote a um Show Específico");
         Ticketman ticketman = new Ticketman();
-        Show show1 = new Show(dataShow1, art1, cache1, despesas1, false);
-        Show show2 = new Show(dataShow2, art2, cache2, despesas2, true);
         ticketman.criarShow(dataShow1, art1, cache1, despesas1, false);
         ticketman.criarShow(dataShow2, art2, cache2, despesas2, true);
-        Lote lote = new Lote("22012024Chappell Roan-0", 25, 10, 65, 0);
-        assertTrue(lote.equals(ticketman.criarLote("22012024Chappell Roan", 25, 10, 65, 0)));
+        Lote lote1 = new Lote("22012024Chappell Roan-0", 25, 10, 65, 0);
+        assertTrue(lote1.equals(ticketman.criarLote("22012024Chappell Roan", 25, 10, 65, 0)));
     }
 
 
