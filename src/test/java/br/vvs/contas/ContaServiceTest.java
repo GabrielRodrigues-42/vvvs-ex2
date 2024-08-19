1efaef94912abc948833ee280de289ec0de650e3
EvaldoBritoJunior <evaldo.brito.junior@ccc.ufcg.edu.br>
2024-08-19 17:43:02 -0300

    add ContaService Tests
    Técnica: Partição por Equivalência

--- src/test/java/br/vvs/contas/ContaServiceTest.java
+++ src/test/java/br/vvs/contas/ContaServiceTest.java
@@ -6,6 +6,7 @@ import br.vvs.contas.model.FaturaStatusEnum;
 import br.vvs.contas.model.TipoPagamentoEnum;
 import br.vvs.contas.service.ContaService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -139,6 +140,120 @@ public class ContaServiceTest {
         service.pagarFatura(fatura, contaII.getCodigo(), dataII, TipoPagamentoEnum.TRANSFERENCIA_BANCARIA);
         assertEquals(fatura.getStatus(), FaturaStatusEnum.PENDENTE);
 
+    }
+
+    @Test
+    @DisplayName("Pagar por cartão")
+    void casoDeTeste11() {
+        ContaService service = new ContaService();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2023, 1, 20, 0, 0, 0);
+        Date data = calendar.getTime();
+        calendar.set(2023, 1, 5, 0, 0, 0);
+        Date dataI = calendar.getTime();
+
+        fatura = new Fatura(data, 1500 ,"TestI");
+        Conta contaI = new Conta(1, data, 1500);
+        ArrayList<Conta> contas = new ArrayList<>(List.of(new Conta[]{contaI}));
+        fatura.setContas(contas);
+
+        service.pagarFatura(fatura, contaI.getCodigo(), dataI, TipoPagamentoEnum.CARTAO_CREDITO);
+        assertEquals(fatura.getStatus(), FaturaStatusEnum.PAGA);
+
+    }
+
+    @Test
+    @DisplayName("Pagar por boleto")
+    void casoDeTeste12() {
+        ContaService service = new ContaService();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2023, 1, 20, 0, 0, 0);
+        Date data = calendar.getTime();
+
+        fatura = new Fatura(data, 1500 ,"TestI");
+        Conta contaI = new Conta(1, data, 1500);
+        ArrayList<Conta> contas = new ArrayList<>(List.of(new Conta[]{contaI}));
+        fatura.setContas(contas);
+
+        service.pagarFatura(fatura, contaI.getCodigo(), data, TipoPagamentoEnum.BOLETO);
+        assertEquals(fatura.getStatus(), FaturaStatusEnum.PAGA);
+    }
+
+    @Test
+    @DisplayName("Pagar por transferencia")
+    void casoDeTeste13() {
+        ContaService service = new ContaService();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2023, 1, 20, 0, 0, 0);
+        Date data = calendar.getTime();
+
+        fatura = new Fatura(data, 1500 ,"TestI");
+        Conta contaI = new Conta(1, data, 1500);
+        ArrayList<Conta> contas = new ArrayList<>(List.of(new Conta[]{contaI}));
+        fatura.setContas(contas);
+
+        service.pagarFatura(fatura, contaI.getCodigo(), data, TipoPagamentoEnum.TRANSFERENCIA_BANCARIA);
+        assertEquals(fatura.getStatus(), FaturaStatusEnum.PAGA);
+    }
+
+    @Test
+    @DisplayName("Pagar por cartão com valor insuficiente")
+    void casoDeTeste14() {
+        ContaService service = new ContaService();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2023, 1, 20, 0, 0, 0);
+        Date data = calendar.getTime();
+        calendar.set(2023, 1, 5, 0, 0, 0);
+        Date dataI = calendar.getTime();
+
+        fatura = new Fatura(data, 1500 ,"TestI");
+        Conta contaI = new Conta(1, data, 500);
+        ArrayList<Conta> contas = new ArrayList<>(List.of(new Conta[]{contaI}));
+        fatura.setContas(contas);
+
+        service.pagarFatura(fatura, contaI.getCodigo(), dataI, TipoPagamentoEnum.CARTAO_CREDITO);
+        assertEquals(fatura.getStatus(), FaturaStatusEnum.PENDENTE);
+
+    }
+
+    @Test
+    @DisplayName("Pagar por boleto com valor insuficente")
+    void casoDeTeste15() {
+        ContaService service = new ContaService();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2023, 1, 20, 0, 0, 0);
+        Date data = calendar.getTime();
+
+        fatura = new Fatura(data, 1500 ,"TestI");
+        Conta contaI = new Conta(1, data, 500);
+        ArrayList<Conta> contas = new ArrayList<>(List.of(new Conta[]{contaI}));
+        fatura.setContas(contas);
+
+        service.pagarFatura(fatura, contaI.getCodigo(), data, TipoPagamentoEnum.BOLETO);
+        assertEquals(fatura.getStatus(), FaturaStatusEnum.PENDENTE);
+    }
+
+    @Test
+    @DisplayName("Pagar por transferência com valor insuficiente")
+    void casoDeTeste16() {
+        ContaService service = new ContaService();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2023, 1, 20, 0, 0, 0);
+        Date data = calendar.getTime();
+
+        fatura = new Fatura(data, 1500 ,"TestI");
+        Conta contaI = new Conta(1, data, 500);
+        ArrayList<Conta> contas = new ArrayList<>(List.of(new Conta[]{contaI}));
+        fatura.setContas(contas);
+
+        service.pagarFatura(fatura, contaI.getCodigo(), data, TipoPagamentoEnum.TRANSFERENCIA_BANCARIA);
+        assertEquals(fatura.getStatus(), FaturaStatusEnum.PENDENTE);
     }
 
 
